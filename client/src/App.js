07509630d8ea439b17ce569f02cdf619ced0692a
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-11-15 08:10:01 +0100

    created pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Safe from './pages/Safe'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Password safe</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to="/Login" />}/>
+          <Route path='/Safe' element={<Safe />}/>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
